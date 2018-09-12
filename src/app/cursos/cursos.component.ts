117f2d09825674600d4ec7a5663ebbdcbf603c86
takato <ltakato@hotmail.com>
2018-09-12 14:35:30 -0300

    aula 06 - templates

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+
+  cursos: string[] = ['Java', 'Python', 'C#'];
+
+  constructor() {
+    this.nomePortal = 'Takato training!';
+  }
 
   ngOnInit() {
   }
